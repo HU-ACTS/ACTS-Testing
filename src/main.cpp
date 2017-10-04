5ca54dcf9b4af1de72461a0a567fc3974fb61075
Ferdi Stoeltie <ferdistoeltie@gmail.com>
2017-10-04 11:19:29 +0200

    added updated files to compile cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,16 @@
 #include "esp_event_loop.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
-
+#include "test.hpp"
+#include <string.h>
 esp_err_t event_handler(void *ctx, system_event_t *event)
 {
     return ESP_OK;
 }
 
-void app_main(void)
+extern "C" void app_main(void)
 {
+    test newTest;
     nvs_flash_init();
     tcpip_adapter_init();
     ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
@@ -20,13 +22,19 @@ void app_main(void)
     ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
     ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
     ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-    wifi_config_t sta_config = {
-        .sta = {
-            .ssid = "access_point_name",
-            .password = "password",
-            .bssid_set = false
-        }
-    };
+    // wifi_config_t sta_config = {
+    //     .sta = {
+    //         .ssid = "access_point_name",
+    //         .password = "password",
+    //         .bssid_set = false
+    //     }
+    // };
+
+    // cxx 11+ intialization
+    wifi_config_t sta_config = {};
+    strcpy((char*)sta_config.sta.ssid, "access_point_name");
+    strcpy((char*)sta_config.sta.password, "password");
+    sta_config.sta.bssid_set = false;
     ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
     ESP_ERROR_CHECK( esp_wifi_start() );
     ESP_ERROR_CHECK( esp_wifi_connect() );
